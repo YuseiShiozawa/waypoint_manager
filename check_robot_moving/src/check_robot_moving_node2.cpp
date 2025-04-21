02b61e7375d860839633164ac8017b44da64d735
Nomura1024 <n.hayato4274@gmail.com>
2025-04-21 17:06:26 +0900

    add stop and next

--- check_robot_moving/src/check_robot_moving_node2.cpp
+++ check_robot_moving/src/check_robot_moving_node2.cpp
@@ -37,7 +37,10 @@ namespace {
     static float limit_time = 20;
     static ros::Time last_cmd_vel_time; //add
     static const double cmd_vel_timeout_sec = 0.5; // cmd_velがこの秒数届かないと停止とみなす
-    
+    static int repeat_waypoint_counter = 0;
+    static const int repeat_waypoint_threshold = 2;
+    static int moving_confirm_count = 0;  // 動いていると判定された回数をカウント
+    static const int moving_confirm_threshold = 50; // 連続で50回動いていたらリセットする
 
 }
 // グローバル変数として定義（関数の外に書く）
@@ -193,7 +196,7 @@ int main(int argc, char **argv) {
 
         if (delta_pose_dist <= limit_delta_pose_dist && !is_reached_goal) {
             ROS_INFO("time:%ld, stopped time:%ld\n", time(NULL) - start_time, time(NULL) - last_moving_time);
-
+            moving_confirm_count = 0;
             if (time(NULL) - last_moving_time >= limit_time) {
                 if (is_fst_waypoint_reached) {
                     std_srvs::Trigger trigger;
@@ -205,8 +208,10 @@ int main(int argc, char **argv) {
                         ROS_INFO("PrevWaypoint call success");
 
                         // 次にNextWaypointを即座に呼ぶ
-                        ros::Duration(5.0).sleep();  // ちょっと待ってから
+                        ros::Duration(3.0).sleep();  // ちょっと待ってから
                         ROS_INFO("Service call NextWaypoint()");
+                        repeat_waypoint_counter++;
+                        ROS_INFO("repeat_waypoint_counter = %d", repeat_waypoint_counter);
                         if (!next_waypoint_service.call(trigger)) {
                             ROS_WARN("Failed to call NextWaypoint");
                         }
@@ -216,10 +221,30 @@ int main(int argc, char **argv) {
 
                     is_to_prev_waypoint.store(true);
                     last_moving_time = time(NULL);
+                    //repeat_waypoint_counter++;
+                    if (repeat_waypoint_counter >= repeat_waypoint_threshold) {
+                        ROS_WARN("Repeated Prev→Next twice, force NextWaypoint");
+            
+                        if (next_waypoint_service.call(trigger)) {
+                            ROS_INFO("Forced NextWaypoint call success");
+                        } else {
+                            ROS_WARN("Forced NextWaypoint call failed");
+                        }
+            
+                        repeat_waypoint_counter = 0;
+                    }
                 }
             }
         } else {
             last_moving_time = time(NULL);
+            //repeat_waypoint_counter = 0;
+            moving_confirm_count++;
+            ROS_INFO("moving_confirm_count: %d", moving_confirm_count);
+
+            if (moving_confirm_count >= moving_confirm_threshold) {
+                // 連続で動けている場合のみリセット
+                repeat_waypoint_counter = 0;
+            }
         }
 
         loop_rate.sleep();
